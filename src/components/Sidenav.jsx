de04fb60d3380b86b630f20078f415e24c0574f4
miguelf <miguelfavioqm@gmail.com>
2023-05-18 00:32:12 -0400

    Probando Components

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaBeer } from 'react-icons/fa';
+
+
+const Sidenav = () => {
+  return (
+    <div>
+       Lets go for a <FaBeer className=' inline-block'/>?
+    </div>
+  )
+}
+
+export default Sidenav
